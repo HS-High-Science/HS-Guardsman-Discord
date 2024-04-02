7afe54fbfabbdccb1bae958a956a9f42673baf7a
Astro <me@astrohweston.xyz>
2024-04-02 21:00:10 +0200

    Made cban a subcommand and added cunban

---
+++ src/bot/commands/moderation/cross/index.ts
@@ -0,0 +1,18 @@
+import { Guardsman } from "index";
+import { ChatInputCommandInteraction } from "discord.js";
+
+export default class ReloadCommand implements ICommand
+{
+    name: Lowercase<string> = "cross";
+    description: string = "Allows Guardsman moderators to cross ban a user from ALL Guardsman-controlled servers.";
+    guardsman: Guardsman;
+
+    constructor(guardsman: Guardsman)
+    {
+        this.guardsman = guardsman;
+    }
+
+    execute(interaction: ChatInputCommandInteraction<"cached">): Promise<void> {
+        return Promise.resolve(undefined);
+    }
+}
