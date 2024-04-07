bdb72c89b7ac6bc476ac7f89723d286e5422405c
Astro <me@astrohweston.xyz>
2024-04-07 22:30:59 +0200

    oops

--- src/bot/commands/verification/updateall.ts
+++ src/bot/commands/verification/updateall.ts
@@ -70,7 +70,7 @@ export default class UpdateAllCommand implements ICommand {
             })
 
             await new Promise((resolve) => {
-                setTimeout(resolve, (Math.floor(Math.random() * 5) * 1000) + 3_000)
+                setTimeout(resolve, 5_000)
             })
         }
 
