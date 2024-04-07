d904d5f6fa9a85bca03fc613d7fb125222707d30
Astro <me@astrohweston.xyz>
2024-04-07 10:11:21 +0200

    did a thing

--- src/bot/commands/developer/clearpendingverifications.ts
+++ src/bot/commands/developer/clearpendingverifications.ts
@@ -17,8 +17,8 @@ export default class ClearPendingVerifications implements ICommand
     {
         await interaction.reply("Clearing...")
 
+        console.log(this.guardsman.database<IVerificationConfirmation>("pending_verification").select("*"));
         this.guardsman.database<IVerificationConfirmation>("pending_verification")
-            .select("*")
             .del();
 
         await interaction.editReply("Cleared!")
